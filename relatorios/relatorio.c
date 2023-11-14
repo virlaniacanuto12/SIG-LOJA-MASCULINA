8983b6a593f634be632234eacf96eccf0efa63f4
virlaniacanuto12 <micarlacanuto12@gmail.com>
2023-11-14 00:00:28 -0300

    Add funções de exibição de relatorio

--- relatorios/relatorio.c
+++ relatorios/relatorio.c
@@ -5,8 +5,8 @@
 void escolhaMenuRelatorio(char escolha){
     switch(escolha){
         case '1':
-            saldoVendasMes();
-        break;
+            relatorioVendedores();
+            break;
         case '2':
             saldoVendasDia();
         break;
@@ -31,17 +31,45 @@ void menuRelatorio(void){
         printf("- - - - - - - - - - - SHOPMEN - - - - - - - - - - - \n");
         printf("____________________________________________________\n");
         printf("                                                    \n");
-        printf("                  RELATÓRIOS                        \n");
+        printf("                     RELATÓRIOS                     \n");
         printf("____________________________________________________\n");
         printf("                                                    \n");
-        printf("             1- Saldo de vendas do mês:             \n");
-        printf("             2- Saldo de vendas do dia:             \n");
-        printf("             3- Destaques vendedores                \n");
+        printf("             1- Relatório Vendedor:                 \n");
+        printf("             2- Relatório Cliente:                  \n");
+        printf("             3- Relatório Mercadoria:               \n");
+        printf("             4- Saldo de vendas do mês:             \n");
+        printf("             5- Saldo de vendas do dia:             \n");
+        printf("             6- Destaques vendedores                \n");
         printf("             0- Voltar                              \n");
         printf("____________________________________________________\n");
         scanf("%c", &charOpcao);
         escolhaMenuRelatorio(charOpcao);
     }while(charOpcao != '0');
+}
+
+void relatorioVendedores(void){
+    char charOpcao;
+    system("clear||cls");
+    printf("____________________________________________________\n");
+    printf("                                                    \n");
+    printf("- - - - - - Loja de Artigos Masculinos - - - - - - -\n");
+    printf(" Developed by @virlaniacanuto12 -- since Aug, 2023  \n");
+    printf("____________________________________________________\n");
+    printf("                                                    \n");
+    printf("- - - - - - - - - - - SHOPMEN - - - - - - - - - - - \n");
+    printf("____________________________________________________\n");
+    printf("                                                    \n");
+    printf("                RELATÓRIOS VENDEDOR                 \n");
+    printf("                                                    \n");
+    printf("               Digite (0) Para Voltar               \n");
+    printf("____________________________________________________\n");
+    printf("                                                    \n");
+    printf("             1- Vendedores ativos:                  \n");
+    printf("             2- Ordem alfabética:                   \n");
+    printf("             3- Listar por naturalidade.            \n");
+    printf("____________________________________________________\n");
+    scanf("%c", &charOpcao);
+    getchar();
 }
 
 void saldoVendasMes(void){
--- relatorios/relatorio.h
+++ relatorios/relatorio.h
@@ -1,5 +1,7 @@
 void saldoVendasMes(void);
 void saldoVendasDia(void);
 void destaquesMes(void);
+
+void relatorioVendedores(void);
 void menuRelatorio(void);
 void escolhaMenuRelatorio(char);
